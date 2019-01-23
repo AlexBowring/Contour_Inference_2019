41e13345c17fce5159e1777154fca8e5d2e74913
AlexBowring <A.bowring@warwick.ac.uk>
2019-01-23 18:36:24 +0000

    fixed bug in the cohens d residuals

--- custom_functions/create_resid.m
+++ custom_functions/create_resid.m
@@ -1,11 +1,14 @@
-function Y = create_resid(observed_data, observed_mean, observed_std, snr_transform)
+function Y = create_resid(observed_data, observed_mean, observed_std, resid_type)
 
 % Generates Signal
 % 	Input:
 %		observed_data:		Array of all subjects observed data.
 %		observed_mean:		Array of the mean of all subjects data.
 %		observed_std:		Array of the standard devations of all subjects data.
-%		snr_transform:      A binary value specifying whether the SNR transformation is applied to the residuals for the cohens d case. 
+%		resid_type:         A value to specify the type of residual -
+%                           0 (default) = standardized residuals (Y - mu^)/std^
+%                           1 = unstandardized residuals (Y - mu^)
+%                           2 = cohens d transformed residuals
 %
 %	Output
 %		Y:					Vector of standardized residuals, of size (prod(dim), nSubj). 
@@ -24,9 +27,11 @@ observed_std = reshape(observed_std, [prod(dim) 1]);
 
 
 % Cohens d case
-if snr_transform == 1
+if resid_type == 2
 	cohen_d = observed_mean./observed_std;
-	Y = ( spdiags(1./observed_std, 0, prod(dim), prod(dim))*resid - cohen_d/2.*(spdiags(1./observed_std, 0, prod(dim), prod(dim))*resid).^2-1)./sqrt(1+cohen_d.^2/2);
-else
+	Y = spdiags(1./observed_std, 0, prod(dim), prod(dim))*resid - cohen_d/2.*((spdiags(1./observed_std, 0, prod(dim), prod(dim))*resid).^2-1);
+elseif resid_type == 1
+    Y = resid;
+else    
 	Y = spdiags(1./observed_std, 0, prod(dim), prod(dim))*resid;
 end 
